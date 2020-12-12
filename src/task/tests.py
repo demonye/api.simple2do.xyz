0d1c82e75a7b885dd5b82271959e8172f528a14a
Eric Ye <eric.ye@facteon.global>
2020-12-13 00:18:51 +1300

    added unit tests

--- src/task/tests.py
+++ src/task/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+import factory
 
-# Create your tests here.
+from .models import TodoTask
+
+
+class TodoTaskFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = TodoTask
+
+    title = factory.Faker('text')
+
+
+class TodoTaskTest(TestCase):
+    """Test TodoTask API"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.tasks = [
+            TodoTaskFactory(),
+            TodoTaskFactory(),
+            TodoTaskFactory(),
+        ]
+
+    def test_get_task_list(self):
+        resp = self.client.get(reverse('task_list_create'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.json()), 3)
+
+
